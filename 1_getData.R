fe0dee06659921b0fabc70c07398bf415fce664f
Patriciatr <patriciatr99@uma.es>
2021-02-05 12:47:09 +0100

    Update 1_getData.R

--- 1_getData.R
+++ 1_getData.R
@@ -1,22 +1,18 @@
 ## Get the RNASeq and the clinical data
 
 
-# set seed to be reproductible
+# set seed to be reproduciblish
 set.seed(12345)
 options(stringsAsFactors = FALSE)
 
 
 source("0_loadLibraries.R")
 
-loadpkg("RTCGAToolbox") # To browse and get TCGA data
-loadpkg("readxl")
-library(RTCGAToolbox)
-library(readxl)
+require("RTCGAToolbox") # To browse and get TCGA data
+require("readxl")
 
 #### Get the breast cancer gene expression data from TCGA
-brcaData <- getFirehoseData(dataset="BRCA", runDate="20160128",gistic2Date="20160128",forceDownload=F, clinical =TRUE, RNASeq2GeneNorm  =TRUE)
-
-brca_rnaseq <- getData(brcaData,type = "RNASeq2GeneNorm")
+load("brca_rnaseq.RData")
 
 #### Gene expression data cleaning up
 ## keep only the tumour data. Sample type 01-09 in the samples barcode means tumour
@@ -35,13 +31,11 @@ save(brca_rnaseq.tumour, file = "brca_rnaseq.RData")
 ## Download Supplementary data and import Table 1
 ## [Supplementary table 1 from Comprehensive molecular portraits of human breast tumours](http://www.nature.com/nature/journal/v490/n7418/full/nature11412.html?foxtrotcallback=true#supplementary-information) has TCGA breast cancer samples classified by PAM50 subtypes and ER Status, PR Status and HER2 Final Status.
 
-temp <- tempfile()
-download.file("https://media.nature.com/original/nature-assets/nature/journal/v490/n7418/extref/nature11412-s2.zip",temp)
-unzip(temp)
-sample_data <- read_excel("nature11412-s2/Supplementary Tables 1-4.xls", sheet = 1, skip = 1)
-unlink("nature11412-s2",recursive = T,force = T)
-rm(temp)
+setwd("/Users/usuario/Documents/Herramientas/BreastCancer")
+
+
+sample_data <- read_excel("Supplementary Tables 1-4.xls", sheet = 1, skip = 1)
+
 
 save(sample_data, file="sample_data.RData")
 
-unlink("20160128*",force = T)
