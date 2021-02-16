b2eb6dd95b84acca345d105e9de1eba9c4b6313d
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2021-02-16 16:15:54 +0100

    Update 2_7_SubsettingPRStatus.R

--- Code/Options/2_7_SubsettingPRStatus.R
+++ Code/Options/2_7_SubsettingPRStatus.R
@@ -1,6 +1,3 @@
-##borrar
-setwd("D:/uma/cuarto/Herramientas y Algoritmos/functional_analysis/flujo cancer")
-
 #cargar lirberias
 source("Code/Settings/0_loadLibraries.R") # no se si hace falta con el bash, creo que no
 loadpkg("dplyr")
@@ -9,7 +6,7 @@ loadpkg("dplyr")
 load("Data/brca_rnaseq_tumour.RData")
 load("Data/sample_data.RData")
 
-print("Lo primero que haremos ser· comprobar si el que el c·ncer tenga receptores de progesterona afecta a la tasa de supervivencia")
+print("Lo primero que haremos ser√° comprobar si el que el cancer tenga receptores de progesterona afecta a la tasa de supervivencia")
 d1 <- sample_data[sample_data$`PR Status`== "Positive",]
 d2 <- sample_data[sample_data$`PR Status`!= "Positive",]
 d2$`PR Status` = "Negative"
@@ -18,7 +15,7 @@ tablaSupervivencia <- xtabs(~datos_limpios$`Vital Status`+ datos_limpios$`PR Sta
 pValor <- unlist(unname(fisher.test(tablaSupervivencia)[1]))
 
 if(pValor<0.05){
-  print(paste("El pvalor es ", round(pValor, 5), ", por lo que se ha encontrado relaciÛn entre ambas variables. Procederemos a efectuar un DEA."))
+  print(paste("El pvalor es ", round(pValor, 5), ", por lo que se ha encontrado relacion entre ambas variables. Procederemos a efectuar un DEA."))
   
   subsettingPR <- function(numNodos){
     samples <- sample_data %>% dplyr::filter(`PR Status` == numNodos)
@@ -43,6 +40,6 @@ if(pValor<0.05){
   preDEA <- list("counts" = counts, "design" = design, "label" = label)
   save(preDEA, file = "Data/datosParaDEA.RData")
 } else {
-  print(paste("El pvalor es ", round(pValor, 4), ", por lo que no se ha encontrado relaciÛn entre ambas variables. El programa se detendr·."))
+  print(paste("El pvalor es ", round(pValor, 4), ", por lo que no se ha encontrado relacion entre ambas variables. El programa se detendra."))
   quit()
 }
