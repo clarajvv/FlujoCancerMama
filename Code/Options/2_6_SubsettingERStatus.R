c6cce44f0843d12feeed1f0be0778161147c9325
IreneSanx <61683259+IreneSanx@users.noreply.github.com>
2021-02-16 16:15:15 +0100

    Update 2_6_SubsettingERStatus.R

--- Code/Options/2_6_SubsettingERStatus.R
+++ Code/Options/2_6_SubsettingERStatus.R
@@ -1,6 +1,3 @@
-##borrar
-setwd("D:/uma/cuarto/Herramientas y Algoritmos/functional_analysis/flujo cancer")
-
 #cargar lirberias
 source("Code/Settings/0_loadLibraries.R") # no se si hace falta con el bash, creo que no
 loadpkg("dplyr")
@@ -9,7 +6,7 @@ loadpkg("dplyr")
 load("Data/brca_rnaseq_tumour.RData")
 load("Data/sample_data.RData")
 
-print("Lo primero que haremos será comprobar si el que el cáncer tenga receptores de estrógenos afecta a la tasa de supervivencia")
+print("Lo primero que haremos sera comprobar si el que el cancer tenga receptores de estrogenos afecta a la tasa de supervivencia")
 d1 <- sample_data[sample_data$`ER Status`== "Positive",]
 d2 <- sample_data[sample_data$`ER Status`!= "Positive",]
 d2$`ER Status` = "Negative"
@@ -18,7 +15,7 @@ tablaSupervivencia <- xtabs(~datos_limpios$`Vital Status`+ datos_limpios$`ER Sta
 pValor <- unlist(unname(fisher.test(tablaSupervivencia)[1]))
 
 if(pValor<0.05){
-  print(paste("El pvalor es ", round(pValor, 5), ", por lo que se ha encontrado relación entre ambas variables. Procederemos a efectuar un DEA."))
+  print(paste("El pvalor es ", round(pValor, 5), ", por lo que se ha encontrado relacion entre ambas variables. Procederemos a efectuar un DEA."))
   
   subsettingER <- function(numNodos){
     samples <- sample_data %>% dplyr::filter(`ER Status` == numNodos)
@@ -43,6 +40,6 @@ if(pValor<0.05){
   preDEA <- list("counts" = counts, "design" = design, "label" = label)
   save(preDEA, file = "Data/datosParaDEA.RData")
 } else {
-  print(paste("El pvalor es ", round(pValor, 4), ", por lo que no se ha encontrado relación entre ambas variables. El programa se detendrá."))
+  print(paste("El pvalor es ", round(pValor, 4), ", por lo que no se ha encontrado relacion entre ambas variables. El programa se detendra."))
   quit()
 }
